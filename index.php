6b868a0cbceff71ec2e70d959706281a7382b8f2
lailiseptiandi <lailiseptiandi2@gmail.com>
2020-07-08 22:40:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+  <title>My Porfolio</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8" />
+  <meta name="keywords" content="Polaroid Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+  <script>
+    addEventListener("load", function() {
+      setTimeout(hideURLbar, 0);
+    }, false);
+
+    function hideURLbar() {
+      window.scrollTo(0, 1);
+    }
+  </script>
+  <!-- Custom Theme files -->
+  <link href="assets/css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+  <link href="assets/css/style.css" type="text/css" rel="stylesheet" media="all">
+  <link rel="stylesheet" href="assets/css/style2.css">
+  <!-- font-awesome icons -->
+  <link href="assets/css/fontawesome-all.min.css" rel="stylesheet">
+  <!-- //Custom Theme files -->
+  <!-- online-fonts -->
+  <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet">
+  <!-- //online-fonts -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+  <script src="assets/js/anime.js"></script>
+</head>
+
+<body>
+  <div class="sidenav">
+    <div class="row side_top">
+      <div class="col-4 side_top_left">
+        <img src="assets/img/about-me.jpg" alt="news image" class="img-fluid navimg">
+      </div>
+      <div class="col-8 side_top_right">
+        <h6>Laili Septiandi </h6>
+        <p>BackEnd Developer</p>
+      </div>
+    </div>
+    <header>
+      <div class="container-fluid px-md-5 ">
+        <nav class="mnu mx-auto">
+          <label for="drop" class="toggle">Menu</label>
+          <input type="checkbox" id="drop">
+          <ul class="menu">
+            <li class="active"><a href="#home" class="scroll">Home</a></li>
+            <li class="mt-sm-3"><a href="#about" class="scroll">About</a></li>
+            <li class="mt-sm-3"><a href="#gallery" class="scroll">Skill</a></li>
+            <li class="mt-sm-3"><a href="#services" class="scroll">Services</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </div>
+  <div class="main" id="home">
+    <div class="banner-text-w3ls">
+      <div class="container">
+        <div class="mx-auto text-left">
+          <h1 class="ml11">
+            <span class="text-wrapper">
+              <span class="line line1"></span>
+              <span class="letters">Halo, Saya Laili Septiandi</span>
+            </span>
+          </h1>
+          <h3 class="mt-3">Seorang Mahasiswa Jogja yang tidak begitu <br> ahli ngoding</h3>
+          <p class="banp mx-auto mt-2">"Pernah menjadi pintar, tapi tak kunjung pintar" </p>
+          <a class="btn btn-primary mt-lg-5 mt-3 agile-link-bnr" href="#about" role="button">Learn More</a>
+        </div>
+      </div>
+    </div>
+    <!-- about -->
+    <section class="slide-wrapper" id="about">
+      <h3 class="w3_head mb-5">About Me </h3>
+      <img src="assets/img/about2.jpg" class="img-fluid" width="60%" alt="">
+      <p class="iner mt-5 text-justify"> Halo, saya <b>Laili Muzzammil Septiadi</b> asal dari Sukabumi, terima kasih telah berkunjung
+        di web saya.
+        Singkat cerita saya seorang mahasiswa yang sedang mendalami web developer dalam bidang BackEnd dengan
+        memakai bahasa PHP,
+        dan memakai teknologi framework <b> Laravel</b>.
+      </p>
+      <table class="table">
+        <h4 style="color: black" class="text-left mb-3 mt-3">I 'am Laili Septiandi</h4>
+        <h5 style="color: black" class="text-left mb-3 mt-3">Biodata :</h5>
+        <tbody>
+          <tr>
+            <td><label>Birthday</label></td>
+            <td>:</td>
+            <td>
+              <p>22 September 1999</p>
+            </td>
+            </>
+          <tr>
+            <td>
+              <label>Email</label>
+            </td>
+            <td>:</td>
+            <td>
+              <p>lailiseptiandi2@gmail.com</p>
+            </td>
+          </tr>
+          <tr>
+            <td><label>Age</label></td>
+            <td>:</td>
+            <td>
+              <p>20 Yr</p>
+            </td>
+          </tr>
+          <tr>
+            <td><label>Phone</label></td>
+            <td>:</td>
+            <td>
+              <p>085798480792</p>
+            </td>
+          </tr>
+          <tr>
+            <td><label>Residence</label></td>
+            <td>:</td>
+            <td>
+              <p>indonesian</p>
+            </td>
+          </tr>
+          <tr>
+            <td><label>Linkedin</label></td>
+            <td>:</td>
+            <td>
+              <p>Laili Septiandi</p>
+            </td>
+          </tr>
+          <tr>
+            <td><label>Address</label></td>
+            <td>:</td>
+            <td>
+              <p>Surade, Kab.Sukabumi</p>
+            </td>
+          </tr>
+          <tr>
+            <td><label>Freelance</label></td>
+            <td>:</td>
+            <td>
+              <p>Available</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <!-- //about -->
+    <!-- news -->
+    <div class="news" id="gallery">
+      <div class="text-center">
+        <h2 class="w3_head">My Skill</h2>
+        <!-- <p class="main_p mb-1  text-justify mx-auto">Masih tahap perkembangan .</p> -->
+      </div>
+      <div class="container2">
+        <div class="bar-1">
+          <div class="title">HTML5</div>
+          <div class="bar" data-width="65%">
+            <div class="bar-inner">
+
+            </div>
+            <div class="bar-percent">65%</div>
+          </div>
+        </div>
+        <div class="bar-1">
+          <div class="title" style="color: #00a3e3;">CSS3</div>
+          <div class="bar" data-width="75%">
+            <div class="bar-inner" style="background: #084c61;">
+
+            </div>
+            <div class="bar-percent">75%</div>
+          </div>
+
+        </div>
+        <div class="bar-1">
+          <div class="title" style="color: #ffd500;">Javascript</div>
+          <div class="bar" data-width="70%">
+            <div class="bar-inner" style="background: #ffd500;">
+
+            </div>
+            <div class="bar-percent">70%</div>
+          </div>
+        </div>
+        <div class="bar-1">
+          <div class="title" style="color: #344057">jQuery</div>
+          <div class="bar" data-width="75%">
+            <div class="bar-inner" style="background: #344057;">
+
+            </div>
+            <div class="bar-percent">75%</div>
+          </div>
+        </div>
+        <div class="bar-1">
+          <div class="title" style="color: #2c6336">Vue Js</div>
+          <div class="bar" data-width="60%">
+            <div class="bar-inner" style="background: #2c6336;">
+
+            </div>
+            <div class="bar-percent">60%</div>
+          </div>
+        </div>
+        <div class="bar-1">
+          <div class="title" style="color: red">Laravel</div>
+          <div class="bar" data-width="80%">
+            <div class="bar-inner" style="background: red;">
+
+            </div>
+            <div class="bar-percent">80%</div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <!-- //news -->
+
+    <!--services-->
+    <div class="wthree_pvtits-services align-w3" id="services">
+      <div class="container">
+        <div class="wthree_pvt_title text-center">
+          <h3 class="w3_head">services
+          </h3>
+          <p class="main_p mb-5  text-center mx-auto">Nulla pellentesque mi non laoreet eleifend. Integer
+            porttitor mollisar curae suspendisse mauris posuere accumsan massa posuere lacus convallis
+            tellus interdum. Amet nullam fringilla nibh nulla convallis ut venenatis purus sit arcu sociis.
+          </p>
+        </div>
+        <div class="wthree_pvtits-services-row row">
+          <div class="col-lg-4 col-md-6 wthree_sgrid py-sm-5 py-4">
+            <span class="fa fa-cog p-sm-4 p-2"></span>
+            <h4 class="my-3">Lorem ipsum dolor </h4>
+          </div>
+          <div class="col-lg-4 col-md-6 wthree_sgrid  py-sm-5 py-4">
+            <span class="fa fa-refresh p-sm-4 p-2"></span>
+            <h4 class="my-3"> sit amet sed diam nonumy </h4>
+          </div>
+          <div class="col-lg-4 col-md-6 wthree_sgrid  py-sm-5 py-4">
+            <span class="fa fa-camera p-sm-4 p-2"></span>
+            <h4 class="my-3">eirmod tempor invidunt</h4>
+          </div>
+          <div class="col-lg-4 col-md-6 wthree_sgrid   py-sm-5 py-4">
+            <span class="fa fa-bell-o p-sm-4 p-2"></span>
+            <h4 class="my-3"> ut labore et dolore </h4>
+          </div>
+          <div class="col-lg-4 col-md-6  wthree_sgrid  py-sm-5 py-4">
+            <span class="fa fa fa-smile-o p-sm-4 p-2"></span>
+            <h4 class="my-3">magna aliquyam erat</h4>
+          </div>
+          <div class="col-lg-4 col-md-6  wthree_sgrid   py-sm-5 py-4">
+            <span class="fa fa-plus-square-o  p-sm-4 p-2"></span>
+            <h4 class="my-3">At vero eos et accusam</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- //services-->
+
+
+
+    <!-- contact -->
+
+
+    <!-- <div class="cpy-right text-center">
+        <div class="follow">
+          <ul class="social_section_1info">
+            <li><a href="https://facebook.com/laili.septiandi2"><span class="fa fa-facebook"></span></a>
+            </li>
+            <li><a href="https://twitter.com/lailiseptiandi"><span class="fa fa-twitter"></span></a></li>
+            <li><a href="https://www.instagram.com/lailiseptiand/"><span class="fa fa-instagram"></span></a>
+            </li>
+            <li><a href="https://id.linkedin.com/in/laili-septiandi-355983172"><span class="fa fa-linkedin"></span></a></li>
+          </ul>
+        </div> -->
+
+    <p class="text-center mb-4">© 2020 Polaroid. All rights reserved | Design by
+      <a href="https://id.linkedin.com/in/laili-septiandi-355983172"> Laili Septiandi.</a>
+    </p>
+    <!-- </div> -->
+    </section>
+    <!-- //contact -->
+
+  </div>
+</body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="assets/js/testAnimate.js"></script>
+
+</html>
